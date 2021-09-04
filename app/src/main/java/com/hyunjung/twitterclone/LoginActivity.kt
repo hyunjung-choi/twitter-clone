872a598d3947ea7cebd9c61d015c6fcf59cfe1bc
hyunjung-choi <hjchoi.dev@gmail.com>
2021-09-05 01:41:34 +0900

    [feat] Firebase Authication으로 로그인 기능 구현

--- app/src/main/java/com/hyunjung/twitterclone/LoginActivity.kt
+++ app/src/main/java/com/hyunjung/twitterclone/LoginActivity.kt
@@ -1,21 +1,93 @@
 package com.hyunjung.twitterclone
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.hyunjung.twitterclone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser?.uid
+        user?.let {
+            startActivity(HomeActivity.newIntent(this))
+            finish()
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setTextChangeListener(binding.emailET, binding.emailTIL)
+        setTextChangeListener(binding.passwordET, binding.passwordTIL)
+
+        // ProgressBar가 떠있는 동안 touch 안되게 막아놓는 역할
+        binding.loginProgressLayout.setOnTouchListener{ v, event -> true }
+    }
+
+    private fun setTextChangeListener(et: EditText, til: TextInputLayout) {
+        et.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                til.isErrorEnabled = false
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     fun onLogin(v: View) {
-        Toast.makeText(this, "onLogin", Toast.LENGTH_SHORT).show()
+        var proceed = true
+        if (binding.emailET.text.isNullOrBlank()) {
+            binding.emailTIL.error = "Email is required"
+            binding.emailTIL.isErrorEnabled = true
+            proceed = false
+        }
+        if (binding.passwordET.text.isNullOrBlank()) {
+            binding.passwordTIL.error = "Password is required"
+            binding.passwordTIL.isErrorEnabled = true
+            proceed = false
+        }
+        if (proceed) {
+            binding.loginProgressLayout.visibility = View.VISIBLE
+            firebaseAuth.signInWithEmailAndPassword(binding.emailET.text.toString(), binding.passwordET.text.toString())
+                .addOnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        binding.loginProgressLayout.visibility = View.GONE
+                        Toast.makeText(this, "Login error: ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+                    binding.loginProgressLayout.visibility = View.GONE
+                }
+        }
     }
 
     fun goToSignup(v: View) {
         Toast.makeText(this, "goToSignup", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
     }
 }
