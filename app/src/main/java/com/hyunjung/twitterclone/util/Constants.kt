39495af28190b6203fbbbe8d6a918dfe1da67541
hyunjung-choi <hjchoi.dev@gmail.com>
2021-09-10 20:46:52 +0900

    [feat] Signup 기능 구현 (#2)
    * [feat] 회원가입 레이아웃 구현
    
    * [feat] User data class 생성
    
    * [feat] User를 Firestore Database에 등록

---
+++ app/src/main/java/com/hyunjung/twitterclone/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.hyunjung.twitterclone.util
+
+val DATA_USERS = "Users"
+val DATA_USER_EMAIL = "email"
+val DATA_USER_USERNAME = "username"
+val DATA_USER_IMAGE_URL = "imageURL"
+val DATA_USERS_HASHTAGES = "followHashtags"
+val DATA_USERS_FOLLOW = "followUsers"
