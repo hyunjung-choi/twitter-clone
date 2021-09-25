3ea02e23120d5c82308940d0941c483de8d2f272
hyunjung-choi <hjchoi.dev@gmail.com>
2021-09-25 17:44:14 +0900

    [feat] Home, Search, MyActivity Fragment 생성

--- app/src/main/java/com/hyunjung/twitterclone/activities/HomeActivity.kt
+++ app/src/main/java/com/hyunjung/twitterclone/activities/HomeActivity.kt
@@ -5,22 +5,68 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.hyunjung.twitterclone.R
+import com.hyunjung.twitterclone.databinding.ActivityHomeBinding
+import com.hyunjung.twitterclone.fragments.HomeFragment
+import com.hyunjung.twitterclone.fragments.MyActivityFragment
+import com.hyunjung.twitterclone.fragments.SearchFragment
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+    private var sectionsPagerAdapter: SectionPagerAdapter? = null
     private val firebaseAuth = FirebaseAuth.getInstance()
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val myActivityFragment = MyActivityFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sectionsPagerAdapter = SectionPagerAdapter(supportFragmentManager)
+
+        // container와 tabs 연결
+        binding.container.adapter = sectionsPagerAdapter
+        binding.container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabs))
+
+        // tabs와 container 연결
+        binding.tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(binding.container))
+        binding.tabs.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
     }
 
     fun onLogout(v: View) {
         firebaseAuth.signOut()
         startActivity(LoginActivity.newIntent(this))
         finish()
+    }
+
+    inner class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getCount() = 3
+
+        override fun getItem(position: Int): Fragment {
+            return when(position) {
+                0 -> homeFragment
+                1 -> searchFragment
+                else -> myActivityFragment
+            }
+        }
+
     }
 
     companion object {
